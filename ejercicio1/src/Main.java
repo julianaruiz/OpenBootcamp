b07b76c0f7df618a1a8a882c572d53436bd1a58d
julianaruiz <julianaruiz152@gmail.com>
2022-05-05 19:13:34 -0500

    ejercicio 1 tema 3

--- ejercicio1/src/Main.java
+++ ejercicio1/src/Main.java
@@ -1,5 +1,17 @@
+//Crear una función con tres parámetros que sean números que se suman entre sí.
+//
+//Llamar a la función en el main y darle valores.
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int resultado = suma(10,7,3);
+        System.out.println(resultado);
+    }
+
+    public static int suma (int a, int b, int c){
+
+        int resultado = a + b + c;
+        return resultado;
     }
 }
